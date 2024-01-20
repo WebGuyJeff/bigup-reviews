377ca20b8afab72ee5eb2a4fab4221c97424c9f7
wordpressjeff <me@jeffersonreal.com>
2024-01-20 10:06:48 +0000

    version bump

--- bigup-reviews.php
+++ bigup-reviews.php
@@ -6,7 +6,7 @@ namespace BigupWeb\Reviews;
  * Description:       A custom 'Reviews' post type with custom meta fields.
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Version:           0.1.1
+ * Version:           0.1.2
  * Author:            Jefferson Real
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
